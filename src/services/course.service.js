ffdec408810936a146947b1d6e368827d1be0647
Pineapple <ccc585858@gmail.com>
2024-04-12 16:34:20 +0800

    change API URL

--- src/services/course.service.js
+++ src/services/course.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/courses/";
+const API_URL = "https://mern-client-g7ak.onrender.com/api/courses/";
 
 class CourseService {
   post(title, description, price) {
